6b922c413777b7999c8bb255bfc978b6beb6a1cc
Marie <marie.zufferey@admin.vs.ch>
2026-04-12 15:35:56 +0200

    first comit

---
+++ docs/conf.py
@@ -0,0 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'AutoGIS'
+copyright = '2024, Kamyar Hasanzadeh, Department of Geosciences & Geography, University of Helsinki'
+author = 'Kamyar Hasanzadeh'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['nbsphinx']
+
+#nbsphinx_execute = 'always'
+nbsphinx_execute = 'auto'  # Execute by default, but respect notebook metadata
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_last_updated_fmt = "%d %B %Y"
+html_logo = "static/logo/AutoGIS.PNG"
+html_favicon = 'static/logo/favicon.ico'
+html_short_title = "AutoGIS"
+html_title = ""
+
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "collapse_navigation": False,
+    'search_bar_position': 'none',
+    'nosidebar': True,
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "notebook_interface": "classic"
+    },
+    "path_to_docs": "docs",
+    "repository_branch": "main",
+    "repository_url": "https://github.com/Automating-GIS-processes/site",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    'logo_only': True,
+    'display_version': False,
+}
+
+#html_static_path = ['static']
+
+nb_execution_mode = "force"
+nb_execution_timeout = 120  # needed, e.g., when matplotlib updates its font cache
+nb_execution_show_tb = True  # show errors
+
+# temp for wfs issue
+#nbsphinx_allow_errors = True
